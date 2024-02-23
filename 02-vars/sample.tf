f1fae2b04c09c2686ddf470b6e51b7a0bb97a667
MuneerBSK <muneerbsk96@gmail.com>
2024-02-23 11:08:20 -0500

    AutoPush

--- 02-vars/sample.tf
+++ 02-vars/sample.tf
@@ -12,3 +12,10 @@ output "sample_output" {
 
 # A variable can be accessed without ${} as well, if it's not accompanied in any sentence. If you just want to use it, you can directly refer it as `var.VariableName`
 
+variable "integer" {
+    default = 100
+}
+
+output "number" {
+    value = var.number
+}
