180e90df6091de6e673d7130a37a789b271745de
MuneerBSK <muneerbsk96@gmail.com>
2024-02-23 11:10:31 -0500

    AutoPush

--- 02-vars/sample.tf
+++ 02-vars/sample.tf
@@ -13,9 +13,22 @@ output "sample_output" {
 # A variable can be accessed without ${} as well, if it's not accompanied in any sentence. If you just want to use it, you can directly refer it as `var.VariableName`
 
 variable "integer" {
-    default = 100
+    default = 1000
 }
 
 output "number" {
     value = var.number
+}
+
+# List variable 
+variable "sample_list" {
+    default = [
+        "Terraform",
+        "Training",
+        "Pulumi",
+        "Cloud Formation",
+        120,
+        true,
+        "ManuVerma"
+    ]
 }
