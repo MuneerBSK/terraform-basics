53c2fdd0952e66b9690342b045153359e491a195
MuneerBSK <muneerbsk96@gmail.com>
2024-02-24 19:18:49 -0500

    AutoPush

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -1,19 +1,18 @@
-# always start with declarringg provider information first
-
-provider "aws" {
-    region = "us-east-1"
-}
-    
+# Block to create EC2
 resource "aws_instance" "web" {
-    ami       = "ami-0ca993affa3ce34e9"
-    instance_type = "t3.nano"
+  ami                       = "ami-0ca993affa3ce34e9"
+  instance_type             = "t3.nano"
+  vpc_security_group_ids    = [aws_security_group.allow_ssh_sg.id]
 
-    tags = {
-        Name = "Terraform-Instance-Server-Name"
-    }
+  tags = {
+    Name        = "Terraform-Instance-Server-Name"
+  }
 }
 
 # Block to print the attribute 
 output "private_dns_of_server" {
     value       = aws_instance.web.private_dns
 }
+
+
+
