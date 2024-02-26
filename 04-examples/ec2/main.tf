b426e6d4003b49c02fc37301542b7233f203d27d
MuneerBSK <muneerbsk96@gmail.com>
2024-02-25 19:42:57 -0500

    AutoPush

--- 04-examples/ec2/main.tf
+++ 04-examples/ec2/main.tf
@@ -1,6 +1,6 @@
 # Block to create EC2
 resource "aws_instance" "web" {
-  ami                       = "ami-0ca993affa3ce34e9"
+  ami                       = data.aws_ami.my_ami.image_id
   instance_type             = "t3.nano"
   vpc_security_group_ids    = [var.sg]
 
