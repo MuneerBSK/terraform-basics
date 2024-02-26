920c2dd1869e6b1d258b6a03e9fb5c9ef6340118
MuneerBSK <muneerbsk96@gmail.com>
2024-02-26 12:52:03 -0500

    AutoPush

--- 04-examples/ec2/main.tf
+++ 04-examples/ec2/main.tf
@@ -8,6 +8,21 @@ resource "aws_instance" "web" {
     Name        = "Terraform-Instance-Server-Name"
   }
 
+# Declaring the remote provisioner inside the resource
+  provisioner "remote-exec" {
+
+      connection {                   // establishes connectivity to the created machine
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = self.private_ip   
+      }
+
+    inline = [
+        "ansible-pull -U https://github.com/MuneerBSK/ansible.git robot-pull.yml -e ENV=dev -e COMPONENT=mongodb"
+    ]
+  }
+
 }
 
 variable "sg" {}
